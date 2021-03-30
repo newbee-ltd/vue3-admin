07a9ee13d137748d06724f40e96bd028c8aabc73
Nick930826 <xianyou1993@qq.com>
2021-03-30 11:31:37 +0800

    update

--- src/views/Introduce.vue
+++ src/views/Introduce.vue
@@ -2,7 +2,7 @@
   <el-card class="account-container">
     <h1>系统简介</h1>
     <div style="line-height: 30px">
-      vue3-admin 是一套企业级后台管理系统，基于 Spring Boot 、Vue 3.0 相关技术栈开发，包含基础版本和前后端分离版本，供各位开发者们体验和学习，一定给你最好的学习体验。
+      vue3-admin 是一套企业级后台管理系统，基于 Spring Boot 、Vue 3.0 相关技术栈开发，供各位开发者们体验和学习，一定给你最好的学习体验。
     </div>
     <img style="width: 70%;
       display: block;
