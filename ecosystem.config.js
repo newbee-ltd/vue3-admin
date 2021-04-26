ec798a187c17d22e8b79b9e2df2c154a27ae203f
Nick930826 <xianyou1993@qq.com>
2021-04-26 14:42:04 +0800

    更新部署文件

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: 'vue3-admin',
+      script: 'vue3-admin-server.js',
+      cwd: 'server'
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'root',
+      host: '47.99.134.126',
+      ref: 'origin/main',
+      repo: 'git@github.com:newbee-ltd/vue3-admin.git',
+      path: '/workspace/vue3-admin',
+      'post-deploy': 'git reset --hard && git checkout main && git pull && npm install && npm run build:release && pm2 startOrReload ecosystem.config.js',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  }
+}
