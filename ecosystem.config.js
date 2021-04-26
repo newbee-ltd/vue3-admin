131355c90580082250ac7ab55620fda70cc7ecb4
Nick930826 <xianyou1993@qq.com>
2021-04-26 15:55:48 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       ref: 'origin/main',
       repo: 'git@git.zhlh6.cn:newbee-ltd/vue3-admin.git',
       path: '/workspace/vue3-admin',
-      'post-deploy': 'git reset --hard && git checkout main && git pull && npm install && npm run build:release && pm2 startOrReload ecosystem.config.js',
+      'post-deploy': 'git reset --hard && git checkout main && git pull && npm i && npm run build:release && pm2 startOrReload ecosystem.config.js',
       env: {
         NODE_ENV: 'production'
       }
