28671b8525f84f02a4e26ac5b54c498b3a68fa3a
Nick930826 <xianyou1993@qq.com>
2021-07-12 10:39:39 +0800

    本地打包dist

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       ref: 'origin/main',
       repo: 'git@git.zhlh6.cn:newbee-ltd/vue3-admin.git',
       path: '/workspace/vue3-admin',
-      'post-deploy': 'git reset --hard && git checkout main && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
+      'post-deploy': 'git reset --hard && git checkout main && git pull && npm i --production=false && pm2 startOrReload ecosystem.config.js', // -production=false 下载全量包
       env: {
         NODE_ENV: 'production'
       }
