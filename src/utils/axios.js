e54926530974f862535459cb5df43365eb3e9793
71F <55472948+Kento97@users.noreply.github.com>
2022-06-23 09:04:15 +0800

    Update axios.js
    修改响应拦截器的注释

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -15,7 +15,7 @@ axios.defaults.headers['token'] = localGet('token') || ''
 // 默认 post 请求，使用 application/json 形式
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
-// 请求拦截器，内部根据返回值，重新组装，统一管理。
+// 响应拦截器，内部根据返回值，重新组装，统一管理。
 axios.interceptors.response.use(res => {
   if (typeof res.data !== 'object') {
     ElMessage.error('服务端异常！')
