1da4079f1184d3056eef71fd8422dafef0261564
Nick930826 <xianyou1993@qq.com>
2023-02-17 10:17:39 +0800

    更新bug

--- src/views/IndexConfig.vue
+++ src/views/IndexConfig.vue
@@ -165,7 +165,7 @@ const handleDelete = () => {
     ElMessage.error('请选择项')
     return
   }
-  axios.post('/indexConfigs/delete', {
+  axios.delete('/indexConfigs', {
     ids: state.multipleSelection.map(i => i.configId)
   }).then(() => {
     ElMessage.success('删除成功')
@@ -174,7 +174,7 @@ const handleDelete = () => {
 }
 // 单个删除
 const handleDeleteOne = (id) => {
-  axios.post('/indexConfigs/delete', {
+  axios.delete('/indexConfigs', {
     ids: [id]
   }).then(() => {
     ElMessage.success('删除成功')
